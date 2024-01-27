a0f03ae632235bdce878e11d39ee789d2488985d
Nyeong <iwbm312@gmail.com>
2024-01-27 18:38:53 +0900

    Design: add Font, AppColor

---
+++ carea/lib/app/common/const/app_colors.dart
@@ -0,0 +1,16 @@
+import "package:flutter/material.dart";
+
+abstract class AppColors {
+  static const greenPrimaryColor = Color(0xFFAED88C);
+  static const bluePrimaryColor = Color(0xFF6BB1ED);
+  static const yellowPrimaryColor = Color(0xFFFCC974);
+  static const redAccentColor = Color(0xFFE55642);
+
+  static const black = Colors.black;
+  static const darkGray = Color(0xFF404040);
+  static const middleGray = Color(0xFF7B7B7D);
+  static const lightGray = Color(0xFF9B9B9B);
+  static const extraLightGray = Color(0xFFD9D9D9);
+  static const faintGray = Color(0xFFF5F5F5);
+  static const white = Colors.white;
+}
