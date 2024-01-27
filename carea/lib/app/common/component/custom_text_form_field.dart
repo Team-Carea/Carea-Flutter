bd2dfc4f3ea747eca7590d75e50f6d30702527b5
Nyeong <iwbm312@gmail.com>
2024-01-27 18:40:17 +0900

    Feat: add common layout, textformfield
    - main.dart: 기본 위젯, 색상, 폰트 적용

---
+++ carea/lib/app/common/component/custom_text_form_field.dart
@@ -0,0 +1,54 @@
+import 'package:carea/app/common/const/app_colors.dart';
+import 'package:flutter/material.dart';
+
+class CustomTextFormField extends StatelessWidget {
+  final String? hintText;
+  final String? errorText;
+  final bool obscureText;
+  final bool autofocus;
+  final ValueChanged<String> onChanged;
+
+  const CustomTextFormField({
+    this.hintText,
+    this.errorText,
+    this.obscureText = false,
+    this.autofocus = false,
+    required this.onChanged,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    const baseBorder = OutlineInputBorder(
+      borderSide: BorderSide(
+        color: AppColors.faintGray,
+        width: 1.0,
+      ),
+    );
+
+    return TextFormField(
+      cursorColor: AppColors.greenPrimaryColor,
+      obscureText: obscureText,
+      autofocus: autofocus,
+      onChanged: onChanged,
+      decoration: InputDecoration(
+        contentPadding: const EdgeInsets.all(20),
+        hintText: hintText,
+        errorText: errorText,
+        hintStyle: const TextStyle(
+          color: AppColors.darkGray,
+          fontSize: 14.0,
+        ),
+        fillColor: AppColors.extraLightGray,
+        filled: true,
+        border: baseBorder,
+        enabledBorder: baseBorder,
+        focusedBorder: baseBorder.copyWith(
+          borderSide: baseBorder.borderSide.copyWith(
+            color: AppColors.greenPrimaryColor,
+          ),
+        ),
+      ),
+    );
+  }
+}
