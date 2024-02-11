96f54e24dc4c7101f3cbef61c661fa780851d1bd
Nyeong <iwbm312@gmail.com>
2024-02-12 07:53:41 +0900

    Design: add logo svg, color, textstyle

---
+++ carea/lib/app/common/const/styles/app_text_style.dart
@@ -0,0 +1,31 @@
+import 'package:carea/app/common/const/app_colors.dart';
+import 'package:flutter/material.dart';
+
+const titleTextStyle = TextStyle(
+  fontSize: 34,
+  fontWeight: FontWeight.w800,
+);
+
+const careaTitleTextStyle = TextStyle(
+  fontSize: 34,
+  fontWeight: FontWeight.w900,
+  color: AppColors.pinkLogoColor,
+);
+
+const inputTitleTextStyle = TextStyle(
+  fontSize: 20,
+  fontWeight: FontWeight.w400,
+  color: AppColors.darkGreenPrimaryColor,
+);
+
+const elevatedBtnTextStyle = TextStyle(
+  color: AppColors.darkGray,
+  fontWeight: FontWeight.w600,
+  fontSize: 18,
+);
+
+const textBtnTextStyle = TextStyle(
+  color: AppColors.darkGray,
+  fontWeight: FontWeight.w400,
+  fontSize: 14,
+);
