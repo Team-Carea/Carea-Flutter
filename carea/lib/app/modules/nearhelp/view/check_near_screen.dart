6741a28fb05f706e668a0eafe65eff8012b6333d
Jeonghyun <limithyunii@gmail.com>
2024-02-12 17:34:36 +0900

    feat: 도움찾기, 도움찾기 확인 구현

---
+++ carea/lib/app/modules/nearhelp/view/check_near_screen.dart
@@ -0,0 +1,96 @@
+// nearhelp_screen -> 도움 올리기
+// check_near_screen -> 올려진 도움 상세 정보
+
+import 'package:carea/app/common/component/progress_bar.dart';
+import 'package:carea/app/common/const/app_colors.dart';
+import 'package:flutter/material.dart';
+
+class NearHelpCheck extends StatefulWidget {
+  const NearHelpCheck({super.key});
+
+  @override
+  State<NearHelpCheck> createState() => _NearHelpCheckState();
+}
+
+class _NearHelpCheckState extends State<NearHelpCheck> {
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: Row(
+        children: [
+          IconButton(
+            onPressed: () => Navigator.pop(context),
+            icon: const Icon(Icons.clear),
+          ),
+        ],
+      ),
+      content: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          const CircleAvatar(
+            backgroundColor: AppColors.darkGray,
+            radius: 50.0,
+          ),
+          const SizedBox(width: 10),
+          const Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 10.0),
+                child: Text(
+                  "캐리아유저 1",
+                  style: TextStyle(fontSize: 20, color: AppColors.black),
+                ),
+              ),
+              // ExpBar 추가
+              Column(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: SizedBox(
+                      width: 350,
+                      height: 30,
+                      child: Text(
+                        '청파동 도움 구합니다~',
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 350,
+                    height: 370,
+                    child: Text(
+                      "오늘 학교 앞에서 저 좀 도아주실분 구합니다 ~오늘 학교 앞에서 저 좀 도아주실분 구합니다 ~오늘 학교 앞에서 저 좀 도아주실분 구합니다 ~오늘 학교 앞에서 저 좀 도아주실분 구합니다 ~ ",
+                      style: TextStyle(fontSize: 16, color: AppColors.black),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          const SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              ElevatedButton(
+                onPressed: () {},
+                child: const Text(
+                  '채팅하기',
+                  style: TextStyle(
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
--- carea/lib/app/modules/nearhelp/view/nearhelp_screen.dart
+++ carea/lib/app/modules/nearhelp/view/nearhelp_screen.dart
@@ -1,5 +1,8 @@
 import 'package:carea/app/common/const/app_colors.dart';
 import 'package:carea/app/common/layout/default_layout.dart';
+import 'package:carea/app/modules/nearhelp/view/check_near_screen.dart';
+import 'package:flutter_chat_ui/flutter_chat_ui.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:flutter/material.dart';
 
 class NearhelpScreen extends StatefulWidget {
@@ -13,17 +16,152 @@ class _NearhelpScreenState extends State<NearhelpScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       appBar: AppBar(
-        backgroundColor: AppColors.greenPrimaryColor,
         title: const Text('도움찾기'),
       ),
       body: const DefaultLayout(
-        child: Center(
-          child: Column(
-            children: [Text('도움찾기')],
-          ),
+        child: GoogleMap(
+          initialCameraPosition:
+              CameraPosition(target: LatLng(31.4354354, 27.4534565)),
+          mapType: MapType.normal,
+          myLocationEnabled: true,
+          myLocationButtonEnabled: true, //사용자 위치 중앙으로 가져오는 버튼
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        elevation: 10.0,
+        onPressed: () {
+          showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return SingleChildScrollView(
+                child: AlertDialog(
+                  title: Row(
+                    children: [
+                      IconButton(
+                          onPressed: () => Navigator.pop(context),
+                          icon: const Icon(Icons.clear)),
+                      const SizedBox(
+                        width: 25,
+                      ),
+                      const Text(
+                        "새 도움 추가하기",
+                        style: TextStyle(fontSize: 20, color: AppColors.black),
+                      ),
+                    ],
+                  ),
+                  content: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      const CircleAvatar(
+                        backgroundColor: AppColors.darkGray,
+                        radius: 50.0,
+                      ),
+                      const Padding(
+                        padding: EdgeInsets.symmetric(vertical: 10.0),
+                        child: Text(
+                          "캐리아유저 1",
+                          style:
+                              TextStyle(fontSize: 20, color: AppColors.black),
+                        ),
+                      ),
+                      SizedBox(
+                        width: 350,
+                        height: 400,
+                        child: Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: AppColors.white,
+                            border: Border.all(color: AppColors.lightGray),
+                          ),
+                          child: Column(
+                            children: [
+                              Container(
+                                padding: const EdgeInsets.all(8.0),
+                                decoration: const BoxDecoration(
+                                  border: Border(
+                                    bottom: BorderSide(
+                                      color: AppColors.lightGray,
+                                      width: 1.0,
+                                    ),
+                                  ),
+                                ),
+                                child: const TextField(
+                                  keyboardType: TextInputType.text,
+                                  textInputAction: TextInputAction.next,
+                                  style: TextStyle(color: AppColors.black),
+                                  decoration: InputDecoration(
+                                    hintText: '제목을 작성해주세요.',
+                                    hintStyle: TextStyle(
+                                      fontSize: 12,
+                                      color: AppColors.lightGray,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                    border: InputBorder.none,
+                                  ),
+                                ),
+                              ),
+                              Expanded(
+                                child: Container(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: const TextField(
+                                    keyboardType: TextInputType.multiline,
+                                    maxLines: null,
+                                    textInputAction: TextInputAction.done,
+                                    style: TextStyle(color: AppColors.black),
+                                    decoration: InputDecoration(
+                                      hintText: '내용을 작성해주세요.',
+                                      hintStyle: TextStyle(
+                                        fontSize: 12,
+                                        color: AppColors.lightGray,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                      border: InputBorder.none,
+                                      contentPadding: EdgeInsets.zero,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  actions: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        ElevatedButton(
+                          onPressed: () {
+                            // 도움 등록 API 전에 UI 확인을 위해 임시적으로 도움찾기 확인하는 UI 연결!!
+                            showDialog(
+                              context: context,
+                              builder: (BuildContext context) {
+                                return const NearHelpCheck();
+                              },
+                            );
+                          },
+                          child: const Text('등록하기',
+                              selectionColor: AppColors.greenPrimaryColor),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              );
+            },
+          );
+        },
+        backgroundColor: AppColors.white,
+        child: const Icon(
+          Icons.add,
+          color: AppColors.greenPrimaryColor,
         ),
       ),
     );
   }
 }
+
+// 추후 지도 배율에 따라서 floating button 색상 수정
