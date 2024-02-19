dfa8abca6b28dfbaaef1da494233545c50e046a8
Nyeong <iwbm312@gmail.com>
2024-02-20 06:20:05 +0900

    Rename: 기본 도움인증화면 이름 변경

---
+++ carea/lib/app/modules/help_confirm/view/helper_confirm_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HelperConfirmScreen extends StatefulWidget {
+  const HelperConfirmScreen({super.key});
+
+  @override
+  State<HelperConfirmScreen> createState() => _HelperConfirmScreenState();
+}
+
+class _HelperConfirmScreenState extends State<HelperConfirmScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ carea/lib/app/modules/help_confirm/view/seeker_confirm_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SeekerConfirmScreen extends StatefulWidget {
+  const SeekerConfirmScreen({super.key});
+
+  @override
+  State<SeekerConfirmScreen> createState() => _SeekerConfirmScreenState();
+}
+
+class _SeekerConfirmScreenState extends State<SeekerConfirmScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
