4b50234a3b2938d0148b5199fecc6496fb98fb7e
Nyeong <iwbm312@gmail.com>
2024-02-21 02:35:49 +0900

    Rename: chat room service
    - 소켓통신 제외한 통신은 전부 여기에 구현..

--- carea/lib/app/data/services/chat_room_service.dart
+++ carea/lib/app/data/services/chat_room_service.dart
@@ -1,50 +1,24 @@
-import 'dart:convert';
+import 'package:dio/dio.dart';
 import 'package:carea/app/common/const/config.dart';
 
 class ChatRoomService {
-  final List<types.Message> messages = [];
-  final user1 = const types.User(
-    id: '2', // 나의 id
-  );
-  final user2 = const types.User(
-    id: '3', // 상대방의 id
-    firstName: '지니신',
-  );
-  bool isLoading = false;
-  late WebSocketChannel channel;
-  final String roomId = '1'; // 임시 채팅방 Id
-  final String accessToken =
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzA4NDQ2MjIxLCJpYXQiOjE3MDg0MzkwMjEsImp0aSI6IjZlY2ZkMWQ3YzE3YzQ3NTQ4NDRiNGMyODU2ZmEwZTJlIiwidXNlcl9pZCI6Mn0.N7PGt424kHXJfX5UCJPEOMhFsC71y0itgWz2fp8dwVY';
+  final Dio dio = Dio();
 
-  // 웹소켓 연결 초기화
-  void initializeWebsocket() {
-    channel = IOWebSocketChannel.connect(
-        'ws://${AppConfig.localHost}${AppConfig.chatRoomUrl}/$roomId?token=$accessToken');
-  }
+  // GET: 채팅방 목록 조회
+  Future<List<Map<String, dynamic>>> getChatRoomList() async {
+    // TODO: develop 브랜치 함수 활용해서 리팩토링
+    // final accessToken = AuthStorage.getAccessToken();
+    const accessToken =
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzA4NDU0MTMyLCJpYXQiOjE3MDg0NDY5MzIsImp0aSI6ImZjNzhkMjA1Mzk0MzQxMjc4NjIxNDNjZmQ2MmU4YThkIiwidXNlcl9pZCI6Mn0.P4jWsv7Ai3ULQv59tpjxttXtzR1DRpnmx1A2Wpg4x0s';
 
-  void addMessage(types.Message message) {
-    messages.insert(0, message); // 화면 맨 아래에 메시지 추가
-    isLoading = true;
-    if (message is types.TextMessage) {
-      // 문제점: 백엔드가 원하는 형태로 보내주지 않음
-      final messagePayload = jsonEncode({
-        'user_id': user1.id,
-        'message': message.text,
-      });
-
-      channel.sink.add(messagePayload);
-      // channel.sink.add(message.text); // 메시지를 백엔드(웹소켓 서버)로 전송
-      messages.insert(
-        0,
-        types.TextMessage(
-          author: user2,
-          createdAt: DateTime.now().millisecondsSinceEpoch,
-          id: const Uuid().v4(),
-          text: "",
-        ),
-      );
-    }
-  }
+    final response = await dio.get(
+      'http://${AppConfig.localHost}/${AppConfig.chatRoomListUrl}/',
+      options: Options(
+        headers: {
+          'Authorization': 'Bearer $accessToken',
+        },
+      ),
+    );
 
     if (response.statusCode == 200) {
       List<dynamic> rawData = response.data;
